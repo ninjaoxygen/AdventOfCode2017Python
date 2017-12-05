fb3b652103c89255acdb3a69b5af412024bd32ac
Chris Poole <chris@avsp.co.uk>
2017-12-05 08:27:32 +0000

    Add day 05

---
+++ day05/day05program1.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+# array of code bytes
+code = []
+
+# instruction pointer
+ip = 0
+
+# step
+step = 0
+
+def readFile(filename):
+    with open(filename) as f:
+        for line in f:
+            code.append(int(line))
+
+def runInstruction():
+    global code
+    global ip
+    global step
+
+    instr = code[ip]
+    code[ip] += 1
+    step += 1
+    ip += instr
+
+def runCode():
+    global code
+    global ip
+    global step
+    
+    print code[0]
+
+    ip = 0
+    step = 0
+    while (ip >= 0) & (ip < len(code)):
+        runInstruction()
+
+code = []
+readFile("sample.txt")
+runCode()
+print "final step was ", step
+
+code = []
+readFile("input.txt")
+runCode()
+print "final step was ", step
